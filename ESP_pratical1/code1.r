23fd34149a1e3a9c47368b89b0a2f42a97cb9a4d
Damon <120879941+Damon0606@users.noreply.github.com>
2023-09-28 15:37:43 +0000

    add new to code1.r

--- ESP_pratical1/code1.r
+++ ESP_pratical1/code1.r
@@ -1,1 +1,3 @@
-setwd()
+setwd("/workspaces/ESP_coursework/ESP_pratical1/Original_Data") ## comment out of submitted
+a <- scan("4300-0.txt", what = "character", skip = 73, nlines = 32858 - 73)
+a <- gsub("_(", "", a, fixed = TRUE) ## remove "_("
