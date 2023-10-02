7fb33031371f0c8390d940a99e836e437aff72ab
Damon <120879941+Damon0606@users.noreply.github.com>
2023-10-02 20:56:31 +0000

    Step 4

--- ESP_pratical1/code1.r
+++ ESP_pratical1/code1.r
@@ -6,15 +6,31 @@ W <- gsub("_(", "", W, fixed = TRUE) ## remove "_("
 
 
 # Step 4
-# punctuation <- c(",", ".", ";", "!", ":", "?")
+### 不能处理一个词中同时出现两个符号的情况
 split_punct <- function(words, punctuation) {
   words_index <- grep("[,.;!:?]", words) # 处理单词结尾是符号[,.;!:?]的情况
   punc_index <- words_index + 1:length(words_index)
   words_modified <- rep("", length(words) + length(punc_index))
   words_modified[-punc_index] <- sapply(words, function(x) ifelse(x %in% words[words_index], substr(x, 1, nchar(x) - 1), x))
   words_modified[punc_index] <- sapply(words[words_index], function(x) substr(x, nchar(x), nchar(x)))
+  
+  words_index_hyphen <- grep("[-—_]", words) # 处理单词开头是符号[-—_]的情况
+  punc_index_hyphen <- words_index_hyphen + 1:length(words_index_hyphen) # 单词应该在的位置
+  words_modified_hyphen <- rep("", length(words) + length(punc_index_hyphen))
+  words_modified[-punc_index_hyphen] <- sapply(words, function(x) ifelse(x %in% words[words_index_hyphen], substr(x, 1, 1), x)) # 放符号
+  words_modified[punc_index_hyphen] <- sapply(words[words_index_hyphen], function(x) substr(x, 2, nchar(x))) # 放单词
   return(words_modified)
 }
+
+
+remove_punctuation <- function(words) {
+  # hyphen_index <- grepl("\\b-+\\w|\\b—+\\w|\\b_+\\w", words) # 处理单词开头包含[-—_]的情况
+  # words[hyphen_index] <- sapply(words[hyphen_index], function(x) substr(x, 2, nchar(x))) # 删除单词开头的[-—_]
+  words <- gsub("[*)]", "", words) # 删除文本数据中的[(*)]
+  words <- words[words != ""]
+  return(words)
+}
+
 
 
 # Step 5
@@ -22,9 +38,9 @@ W_splited <- split_punct(W, punctuation)
 W_splited
 
 remove_punctuation <- function(words) {
-  hyphen_index <- grepl("\\b-+\\w|\\b—+\\w|\\b_+\\w", words) # 处理单词开头包含[-—_]的情况
-  words[hyphen_index] <- sapply(words[hyphen_index], function(x) substr(x, 2, nchar(x))) # 删除单词开头的[-—_]
-  words <- gsub("[,.;!:?_]", "", words) # 删除所有分隔出来的[,.;!:?_]
+  # hyphen_index <- grepl("\\b-+\\w|\\b—+\\w|\\b_+\\w", words) # 处理单词开头包含[-—_]的情况
+  # words[hyphen_index] <- sapply(words[hyphen_index], function(x) substr(x, 2, nchar(x))) # 删除单词开头的[-—_]
+  words <- gsub("[*)]", "", words) # 删除文本数据中的[(*)]
   words <- words[words != ""]
   return(words)
 }
