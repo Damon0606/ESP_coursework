b0913b9724fe3e861e057cca6d09af88d0089155
shiyuqi824 <145208175+shiyuqi824@users.noreply.github.com>
2023-10-12 13:09:11 +0000

    add timetable

--- ESP_pratical2/proj2.R
+++ ESP_pratical2/proj2.R
@@ -1,12 +1,15 @@
 set.seed(123)
 qsim <- function(mf,mb,a.rate,trb,trf,tmb,tmf,maxb){
   car_timetable <- sample(c(0, 1), size=2*60*60, replace = TRUE, prob=c(0.9, 0.1))
-  french_time <- round(runif(n=2*60*60, min=tmf, max=tmf+trf), 0)
-  british_time <- round(runif(n=2*60*60, min=tmb, max=tmb+trb), 0)
-  return (cbind(car_timetable, french_time, british_time))
+  french_time<- matrix(nrow = 5,ncol = 2*60*60)
+  british_time<- matrix(nrow = 5,ncol = 2*60*60)                     
+  for (i in 1:5) {
+    french_time[i,]=round(runif(n=2*60*60, min=tmf, max=tmf+trf), 0)
+    british_time[i,] <- round(runif(n=2*60*60, min=tmb, max=tmb+trb), 0)
+  }
+  return (list(car_timetable, french_time, british_time))
 }
 qsim(5,5,.1,40,40,30,30,20)
-
 
 
 
