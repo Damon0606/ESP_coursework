c523ace27666e6e8b92e615d0fe5552b4cd7a2fb
Damon <120879941+Damon0606@users.noreply.github.com>
2023-11-10 17:26:57 +0000

    1

--- ESP_practical4/practical_4_draft.R
+++ ESP_practical4/practical_4_draft.R
@@ -28,6 +28,7 @@ nn = netup(c(4, 8, 7, 3))
 # b: 4 layers-(4-1) offset parameters
 # b^1: 1*8; b^2: 1*7; b^3: 1*3
 
+
 #———————————————————————————————————————————————————————————————————————————————
 # forward函数：计算网络中每个节点的值
 # nn: a network list as returned by "netup"
@@ -45,3 +46,38 @@ forward <- function(nn, inp) {
 
 forward(nn, inp)
 
+
+#———————————————————————————————————————————————————————————————————————————————
+# backward函数：计算损失函数对节点、权重和偏置的导数
+# nn: the return from "forward"
+# k: the loss corresponding to output class k for network nn
+backward <- function(nn, k) {
+  n <- length(nn$h)
+  nn$dh <- vector("list", n)
+  nn$dW <- vector("list", n-1)
+  nn$db <- vector("list", n-1)
+  
+  # Loss
+  # L = -sum(log(nn$dh[[n]])/n)
+  
+  # Compute the derivative of the loss for k_i w.r.t. h^L_j
+  
+  # For classes that j ≠ k_i, the derivative remains unchanged.
+  nn$dh[[n]] <- exp(nn$h[[n]]) / sum(exp(nn$h[[n]]))
+  # For the correct class (i.e., j = k_i), we need to subtract 1 from its derivative
+  # Ensure that only the output nodes corresponding to the correct class contribute to the derivative of the loss function.
+  nn$dh[[n]][k] <- nn$dh[[n]][k] - 1
+  
+  # 反向传播计算导数
+  for (l in (n-1):2) {
+    nn$dh[[l]] <- ifelse(nn$h[[l]] > 0, nn$W[[l]] %*% nn$dh[[l+1]], 0)
+  }
+  
+  # 计算权重和偏置的导数
+  for (l in 1:(n-1)) {
+    nn$dW[[l]] <- nn$dh[[l+1]] %*% t(nn$h[[l]])
+    nn$db[[l]] <- nn$dh[[l+1]]
+  }
+  
+  return(nn)
+}
